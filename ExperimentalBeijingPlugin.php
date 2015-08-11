318c27062d46ba11426a278e6d83e9823c09d0ff
Zach Ploskey <zach@ploskey.com>
2015-08-11 09:25:49 -0700

    Basic plugin with loaded translations.
    Worry about setting up all the database tables later.
    Currently there is only one instance where they have been
    manually added.

--- ExperimentalBeijingPlugin.php
+++ ExperimentalBeijingPlugin.php
@@ -1,43 +1,25 @@
 <?php
 
-class ExperimentalBeijing extends Omeka_Plugin_AbstractPlugin
+/**
+ * Extensions for the Experimental Beijing project.
+ */
+
+/**
+ * Experimental Beijing plugin main class.
+ */
+class ExperimentalBeijingPlugin extends Omeka_Plugin_AbstractPlugin
 {
-    protected $_pluginPrefix = 'ebj_';
-    public $prefix;
 
     protected $_hooks = array(
-        'install',
-        'uninstall',
         'initialize',
     );
 
-    public function __construct()
-    {
-        parent::__construct();
-        $db = $this->_db;
-        $this->prefix = $db->prefix . $this->_pluginPrefix;
-    }
-
-    public function hookInstall()
-    {
-        $db = $this->_db;
-//        $db->query("
-//            CREATE TABLE IF NOT EXISTS `{$this->prefix}work`"
-//        );
-    }
-
-    public function hookUninstall()
-    {
-        $db = $this->_db;
-//        $db->query("DROP TABLE IF EXISTS `{$this->prefix}work;");
-    }
-
     /**
-     * Insert our custom item types.
+     * Initialize translations.
      */
     public function hookInitialize()
     {
-        $db = $this->_db;
-//        $db->query("INSERT INTO `{$this->prefix}work");
+        add_translation_source(dirname(__FILE__) . '/languages');
     }
+
 }
