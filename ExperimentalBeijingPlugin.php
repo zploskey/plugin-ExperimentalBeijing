f6ee599cddfea0da70bff9281a7c0c9223a3481b
Zach Ploskey <zach@ploskey.com>
2016-12-02 14:16:12 -0800

    Add Language selector to public navigation
    Add a language selector to nav that reloads the current
    page with the GET parameter ?lang=en_US or ?lang=zh_CN
    so that the user can pick English or Chinese.
    
    This incorporates with our fork of the Multilanguage plugin
    to switch the language displayed to the user.

--- ExperimentalBeijingPlugin.php
+++ ExperimentalBeijingPlugin.php
@@ -14,12 +14,31 @@ class ExperimentalBeijingPlugin extends Omeka_Plugin_AbstractPlugin
         'initialize',
     );
 
+    protected $_filters = array(
+        'public_navigation_main',
+    );
+
     /**
      * Initialize translations.
      */
     public function hookInitialize()
     {
         add_translation_source(dirname(__FILE__) . '/languages');
+    }
+
+    public function filterPublicNavigationMain($nav)
+    {
+        $nav[] = array(
+            'label' => 'Language',
+            'uri' => current_url(),
+            'pages' => array(
+                array('label' => '中文',
+                      'uri' => current_url(array('lang' => 'zh_CN'))),
+                array('label' => 'English',
+                      'uri' => current_url(array('lang' => 'en_US'))),
+            ),
+        );
+        return $nav;
     }
 
 }
