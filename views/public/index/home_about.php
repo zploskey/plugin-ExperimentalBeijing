08f2a6e091c7ed1548baa83fdcdd2f310fb1d46f
Zach Ploskey <zach@ploskey.com>
2018-02-22 23:29:28 -0800

    Update home page about text

--- views/public/index/home_about.php
+++ views/public/index/home_about.php
@@ -1,15 +1,8 @@
 <p>
 <?php echo __(
-"<em>Experimental Beijing: Gender and Globalization in Chinese Contemporary
-Art</em> (Duke University Press, 2018) is a book by Sasha Su-Ling Welland.
-Grounded in ethnographic and visual interpretation, it explores Chinese
-contemporary art at the turn of the millennium as a zone of cultural encounter
-— between ideas of masculinity and femininity, public and private, rural and
-urban, China and the world. A feminist lens provides insight into the
-interlocking power dynamics involved in the shifting role of art during an era
-of experimentation. This digital companion, designed to complement and visually
-deepen the experience of reading the book, features expanded multimedia
-materials and provides readers with a nonlinear way to explore the image worlds
-it proposes."
+"This digital companion to the book <em>Experimental Beijing</em> by Sasha
+Welland explores Chinese contemporary art as a zone of cultural
+encounter—between ideas of masculinity and femininity, public and private,
+rural and urban, China and the world."
 ); ?>
 </p>
