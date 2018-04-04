33faaaf83087bc61c031fd4c1a3ae22cab03b958
Zach Ploskey <zach@ploskey.com>
2018-04-03 22:30:37 -0700

    Updates to chapter listing html
    Coinciding with theme update for large screens.

--- views/public/chapter/index.php
+++ views/public/chapter/index.php
@@ -8,53 +8,72 @@ echo head(array(
 
 <h1><?php echo $title; ?></h1>
 
-<ul class="chapters">
-    <?php $i = 1; ?>
-    <li class="part-title"><?php echo __('Prologue'); ?></li>
-    <li class="part-subtitle">
+<div class="chapters">
+  <?php $i = 1; ?>
+  <div class="part">
+    <ul class="chapter-list only">
+      <li class="chapter-number"><?php echo __('Prologue'); ?></li>
+      <li class="chapter-name">
         <a href="<?php echo public_url('exhibits/show/worldly-fables'); ?>">
         <?php echo __('Worldly Fables'); ?>
         </a>
-    </li>
-    <li class="part-title"><?php echo __('Introduction'); ?></li>
-    <li class="part-subtitle">
+      </ul>
+    </ul>
+  </div>
+  <div class="part">
+    <ul class="chapter-list only">
+      <li class="chapter-number"><?php echo __('Introduction'); ?></li>
+      <li class="chapter-name">
         <a href="<?php echo public_url('exhibits/show/chinese-art-expanded'); ?>">
         <?php echo __('Chinese Art in the Expanded Field'); ?>
         </a>
-    </li>
-    <li class="part-title"><?php echo __('Part I'); ?></li>
-    <li class="part-subtitle"><?php echo __('Art Worldings'); ?></li>
-    <ul class="chapter-list">
-        <li class="chapter-number"><?php echo __('Chapter %d', $i++); ?></li>
-        <li class="chapter-name">
-            <a href="<?php echo public_url('exhibits/show/xianfeng-beijing'); ?>">
-            <?php echo __('<em>Xianfeng</em> Beijing'); ?>
-            </a>
-        </li>
-        <li class="chapter-number"><?php echo __('Chapter %d', $i++); ?></li>
-        <li class="chapter-name">
-            <a href="<?php echo public_url('exhibits/show/showcase-beijing'); ?>">
-            <?php echo __('Showcase Beijing'); ?></li>
-            </a>
+      </li>
     </ul>
-    <li class="part-title"><?php echo __('Part II'); ?></li>
-    <li class="part-subtitle"><?php echo __('Zones of Encounter'); ?></li>
+  </div>
+  <div class="part">
+    <div class="title">
+      <div class="part-title"><?php echo __('Part I'); ?></div>
+      <div class="part-subtitle"><?php echo __('Art Worldings'); ?></div>
+    </div>
     <ul class="chapter-list">
-        <li class="chapter-number"><?php echo __('Chapter %d', $i++); ?></li>
-        <li class="chapter-name">
-            <a href="<?php echo public_url('exhibits/show/besieged-city'); ?>">
-            <?php echo __('The Besieged City'); ?>
-            </a>
-        </li>
-        <li class="chapter-number"><?php echo __('Chapter %d', $i++); ?></li>
-        <li class="chapter-name">
-            <a href="<?php echo public_url('exhibits/show/hinterlands-feminist-art'); ?>">
-            <?php echo __('The Hinterlands of Feminist Art'); ?>
-            </a>
-        </li>
+      <li class="chapter-number"><?php echo __('Chapter %d', $i++); ?></li>
+      <li class="chapter-name">
+          <a href="<?php echo public_url('exhibits/show/xianfeng-beijing'); ?>">
+          <?php echo __('<em>Xianfeng</em> Beijing'); ?>
+          </a>
+      </li>
+      <li class="chapter-number"><?php echo __('Chapter %d', $i++); ?></li>
+      <li class="chapter-name">
+          <a href="<?php echo public_url('exhibits/show/showcase-beijing'); ?>">
+          <?php echo __('Showcase Beijing'); ?></a>
+      </li>
     </ul>
-    <li class="part-title"><?php echo __('Part III'); ?></li>
-    <li class="part-subtitle"><?php echo __('Feminist Sightlines'); ?></li>
+  </div>
+  <div class="part">
+    <div class="title">
+      <div class="part-title"><?php echo __('Part II'); ?></div>
+      <div class="part-subtitle"><?php echo __('Zones of Encounter'); ?></div>
+    </div>
+    <ul class="chapter-list">
+      <li class="chapter-number"><?php echo __('Chapter %d', $i++); ?></li>
+      <li class="chapter-name">
+          <a href="<?php echo public_url('exhibits/show/besieged-city'); ?>">
+          <?php echo __('The Besieged City'); ?>
+          </a>
+      </li>
+      <li class="chapter-number"><?php echo __('Chapter %d', $i++); ?></li>
+      <li class="chapter-name">
+          <a href="<?php echo public_url('exhibits/show/hinterlands-feminist-art'); ?>">
+          <?php echo __('The Hinterlands of Feminist Art'); ?>
+          </a>
+      </li>
+    </ul>
+  </div>
+  <div class="part">
+    <div class="title">
+      <div class="part-title"><?php echo __('Part III'); ?></div>
+      <div class="part-subtitle"><?php echo __('Feminist Sight Lines'); ?></div>
+    </div>
     <ul class="chapter-list">
         <li class="chapter-number"><?php echo __('Chapter %d', $i++); ?></li>
         <li class="chapter-name">
@@ -75,10 +94,18 @@ echo head(array(
             </a>
         </li>
     </ul>
-    <li class="part-title"><?php echo __('Epilogue'); ?></li>
-    <li class="part-subtitle">
-        <a href="<?php echo public_url('exhibits/show/recursive-worldly-fables'); ?>">
-        <?php echo __('Recursive Worldly Fables'); ?>
-        </a>
-    </li>
-</ul>
+  </div>
+  <div class="part">
+    <ul class="chapter-list only">
+      <li class="chapter-number"><?php echo __('Epilogue'); ?></li>
+        <li class="chapter-name">
+            <a href="<?php echo public_url('exhibits/show/recursive-worldly-fables'); ?>">
+            <?php echo __('Recursive Worldly Fables'); ?>
+            </a>
+        </li>
+      </ul>
+    </ul>
+  </div>
+</div>
+
+<br/><br/><br/><br/>
