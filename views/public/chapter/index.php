19814cb0d96ad4ff9719128acbeac2e7ae8dbe12
Zach Ploskey <zach@ploskey.com>
2018-05-11 09:53:35 -0700

    Translate whole "Chapter X" phrases individually
    Lets us use Chinese numerals in the translation.
    Better handle Chapter texts that are currently being entered as
    "Chapter 1", "Chapter 2", "Introduction", etc.
    Allows us to make these a controlled vocabulary in the admin so
    they appear in dropdown menus when being entered.

--- views/public/chapter/index.php
+++ views/public/chapter/index.php
@@ -9,7 +9,6 @@ echo head(array(
 <h1><?php echo $title; ?></h1>
 
 <div class="chapters">
-  <?php $i = 1; ?>
   <div class="part">
     <ul class="chapter-list only">
       <li class="chapter-number"><?php echo __('Prologue'); ?></li>
@@ -36,13 +35,13 @@ echo head(array(
       <div class="part-subtitle"><?php echo __('Art Worldings'); ?></div>
     </div>
     <ul class="chapter-list">
-      <li class="chapter-number"><?php echo __('Chapter %d', $i++); ?></li>
+      <li class="chapter-number"><?php echo __('Chapter 1'); ?></li>
       <li class="chapter-name">
           <a href="<?php echo public_url('exhibits/show/xianfeng-beijing'); ?>">
           <?php echo __('<em>Xianfeng</em> Beijing'); ?>
           </a>
       </li>
-      <li class="chapter-number"><?php echo __('Chapter %d', $i++); ?></li>
+      <li class="chapter-number"><?php echo __('Chapter 2'); ?></li>
       <li class="chapter-name">
           <a href="<?php echo public_url('exhibits/show/showcase-beijing'); ?>">
           <?php echo __('Showcase Beijing'); ?></a>
@@ -55,13 +54,13 @@ echo head(array(
       <div class="part-subtitle"><?php echo __('Zones of Encounter'); ?></div>
     </div>
     <ul class="chapter-list">
-      <li class="chapter-number"><?php echo __('Chapter %d', $i++); ?></li>
+      <li class="chapter-number"><?php echo __('Chapter 3'); ?></li>
       <li class="chapter-name">
           <a href="<?php echo public_url('exhibits/show/besieged-city'); ?>">
           <?php echo __('The Besieged City'); ?>
           </a>
       </li>
-      <li class="chapter-number"><?php echo __('Chapter %d', $i++); ?></li>
+      <li class="chapter-number"><?php echo __('Chapter 4'); ?></li>
       <li class="chapter-name">
           <a href="<?php echo public_url('exhibits/show/hinterlands-feminist-art'); ?>">
           <?php echo __('The Hinterlands of Feminist Art'); ?>
@@ -75,19 +74,19 @@ echo head(array(
       <div class="part-subtitle"><?php echo __('Feminist Sight Lines'); ?></div>
     </div>
     <ul class="chapter-list">
-        <li class="chapter-number"><?php echo __('Chapter %d', $i++); ?></li>
+        <li class="chapter-number"><?php echo __('Chapter 5'); ?></li>
         <li class="chapter-name">
             <a href="<?php echo public_url('exhibits/show/red-detachment'); ?>">
             <?php echo __('Red Detachment'); ?>
             </a>
         </li>
-        <li class="chapter-number"><?php echo __('Chapter %d', $i++); ?></li>
+        <li class="chapter-number"><?php echo __('Chapter 6'); ?></li>
         <li class="chapter-name">
             <a href="<?php echo public_url('exhibits/show/opening-great-wall'); ?>">
             <?php echo __('Opening the Great Wall'); ?>
             </a>
         </li>
-        <li class="chapter-number"><?php echo __('Chapter %d', $i++); ?></li>
+        <li class="chapter-number"><?php echo __('Chapter 7'); ?></li>
         <li class="chapter-name">
             <a href="<?php echo public_url('exhibits/show/camouflaged-histories'); ?>">
             <?php echo __('Camouflaged Histories'); ?>
